c62daeebf7d9f5efcbfdb322ba038e74193c314e
Deepak Bitragunta <deepakbvs@gmail.com>
2015-03-06 18:42:25 -0600

    Added code for tblastn and blastx advanced settings and fixed a bug reltaed root URL

--- blast_ui/theme/blast_user_menupage.tpl.php
+++ blast_ui/theme/blast_user_menupage.tpl.php
@@ -28,23 +28,23 @@ database type. Please click on the program name to view the search form.
   <tr>
     <td rowspan = "2">Nucleotide</td> 
     <td>Nucleotide</td> 
-    <td><?php print l('blastn', './blast/nucleotide/nucleotide');?>:
+    <td><?php print l('blastn', 'blast/nucleotide/nucleotide');?>:
       Search a nucleotide database using a nucleotide query.</td>
   </tr>
   <tr>
     <td>Protein</td>
-    <td><?php print l('blastx', './blast/nucleotide/protein');?>:
+    <td><?php print l('blastx', 'blast/nucleotide/protein');?>:
       Search protein database using a translated nucleotide query.</td>
   </tr>
   <tr>
     <td rowspan="2">Protein</td>
     <td>Nucleotide</td> 
-    <td><?php print l('tblastn', './blast/protein/nucleotide');?>:
+    <td><?php print l('tblastn', 'blast/protein/nucleotide');?>:
       Search translated nucleotide database using a protein query.</td>
   </tr>
   <tr>
     <td>Protein</td>
-    <td><?php print l('blastp', './blast/protein/protein');?>:
+    <td><?php print l('blastp', 'blast/protein/protein');?>:
       Search protein database using a protein query.</td>
   </tr> 
 </table>
